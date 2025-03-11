fb47bc890d70bf157c9d00302d151f15634591f1
Zulfan Faizun Najib <121880142+faeznz@users.noreply.github.com>
2025-03-11 21:06:38 +0700

    feat: fix layout zakat fitrah

--- src/page/zakat/fitrah/ZakatFitrahPage.jsx
+++ src/page/zakat/fitrah/ZakatFitrahPage.jsx
@@ -83,86 +83,99 @@ const ZakatFitrahPage = () => {
     },);
 
     return (
-        <div>
+        <div className="flex flex-row w-screen h-screen">
             <NavbarComp />
-            <h2>Data Zakat Fitrah {hijriYear}</h2>
+            <div className="w-full p-12 text-xl text-[#2C368B]">
+                <div class="flex mb-4">
+                    <div class="w-full bg-[#2C368B] text-white font-extrabold h-24 rounded-2xl flex justify-center items-center text-3xl">Zakat Fitrah Masjid Trayu {hijriYear} H</div>
+                </div>
 
-            {formError && <p style={{ color: "red" }}>{formError}</p>}
+                <div className="flex flex-row">
+                    {formError && <p style={{ color: "red" }}>{formError}</p>}
 
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="nama">Nama:</label>
-                <input
-                    type="text"
-                    id="nama"
-                    value={formData.nama}
-                    onChange={(e) => setFormData({ ...formData, nama: e.target.value })}
-                    required
-                />
+                    <form onSubmit={handleSubmit} className="flex flex-col w-1/2 p-8 gap-4">
+                        <label htmlFor="nama">Nama:</label>
+                        <input
+                            type="text"
+                            id="nama"
+                            value={formData.nama}
+                            onChange={(e) => setFormData({ ...formData, nama: e.target.value })}
+                            required
+                            className="border-2 rounded-full py-2 px-6"
+                        />
 
-                <label htmlFor="alamat">Alamat:</label>
-                <select
-                    id="alamat"
-                    value={formData.alamat}
-                    onChange={(e) => setFormData({ ...formData, alamat: e.target.value })}
-                    required
-                >
-                    <option value="Derpoyudan">Derpoyudan</option>
-                    <option value="Kauman">Kauman</option>
-                    <option value="Sorobayan">Sorobayan</option>
-                </select>
+                        <label htmlFor="alamat">Alamat:</label>
+                        <select
+                            id="alamat"
+                            value={formData.alamat}
+                            onChange={(e) => setFormData({ ...formData, alamat: e.target.value })}
+                            required
+                            className="border-2 rounded-full py-2 px-6"
+                        >
+                            <option value="Derpoyudan">Derpoyudan</option>
+                            <option value="Kauman">Kauman</option>
+                            <option value="Sorobayan">Sorobayan</option>
+                        </select>
 
-                <label htmlFor="jiwa">Jiwa:</label>
-                <input
-                    type="number"
-                    id="jiwa"
-                    value={formData.jiwa}
-                    onChange={(e) => setFormData({ ...formData, jiwa: e.target.value })}
-                    required
-                />
+                        <label htmlFor="jiwa">Jiwa:</label>
+                        <input
+                            type="number"
+                            id="jiwa"
+                            value={formData.jiwa}
+                            onChange={(e) => setFormData({ ...formData, jiwa: e.target.value })}
+                            required
+                            className="border-2 rounded-full py-2 px-6"
+                        />
 
-                <label htmlFor="jumlah">Jumlah:</label>
-                <input
-                    type="number"
-                    id="jumlah"
-                    value={formData.jumlah}
-                    onChange={(e) => setFormData({ ...formData, jumlah: e.target.value })}
-                />
+                        <label htmlFor="jumlah">Jumlah:</label>
+                        <input
+                            type="number"
+                            id="jumlah"
+                            value={formData.jumlah}
+                            onChange={(e) => setFormData({ ...formData, jumlah: e.target.value })}
+                            className="border-2 rounded-full py-2 px-6"
+                        />
 
-                <label htmlFor="uang">Uang:</label>
-                <input
-                    type="number"
-                    id="uang"
-                    value={formData.uang}
-                    onChange={(e) => setFormData({ ...formData, uang: e.target.value })}
-                />
+                        <label htmlFor="uang">Uang:</label>
+                        <input
+                            type="number"
+                            id="uang"
+                            value={formData.uang}
+                            onChange={(e) => setFormData({ ...formData, uang: e.target.value })}
+                            className="border-2 rounded-full py-2 px-6"
+                        />
 
-                <button type="submit">Submit</button>
-            </form>
+                        <button type="submit" className="rounded-full py-4 mt-4 text-white bg-[#2C368B]" >Tambah</button>
+                    </form>
 
-            <table>
-                <thead>
-                    <tr>
-                        <th>No</th>
-                        <th>Nama</th>
-                        <th>Alamat</th>
-                        <th>Jiwa</th>
-                        <th>Jumlah</th>
-                        <th>Uang</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {data?.map((item, index) => ( // Tambahkan index
-                        <tr key={index}>
-                            <td>{index + 1}</td> {/* Gunakan index + 1 */}
-                            <td>{item.nama}</td>
-                            <td>{item.alamat}</td>
-                            <td>{item.jiwa}</td>
-                            <td>{item.jumlah}</td>
-                            <td>{item.uang}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+                    <div className="w-1/2 p-8 overflow-y-auto">
+                        <table className="w-full">
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Nama</th>
+                                    <th>Alamat</th>
+                                    <th>Jiwa</th>
+                                    <th>Jumlah</th>
+                                    <th>Uang</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {data?.map((item, index) => ( 
+                                    <tr key={index}>
+                                        <td>{index + 1}</td> 
+                                        <td>{item.nama}</td>
+                                        <td>{item.alamat}</td>
+                                        <td>{item.jiwa}</td>
+                                        <td>{item.jumlah}</td>
+                                        <td>{item.uang}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
